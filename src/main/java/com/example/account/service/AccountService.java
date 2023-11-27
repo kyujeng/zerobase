0ddb7e2e5d31cc4e10752d280af4d3cd059f2d29
김유정 <yjtl2017@gmail.com>
2023-11-27 21:32:02 +0900

    2023.11.27_계좌 생성 API 001

---
+++ src/main/java/com/example/account/service/AccountService.java
@@ -0,0 +1,28 @@
+package com.example.account.service;
+
+import com.example.account.domain.Account;
+import com.example.account.domain.AccountStatus;
+import com.example.account.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    @Transactional
+    public void createAccount(Long userId, Long initialBalance) {
+
+    }
+
+    @Transactional
+    public Account getAccount(Long id) {
+        if(id < 0){
+            throw new RuntimeException("Minus");
+        }
+        return accountRepository.findById(id).get();
+    }
+}
